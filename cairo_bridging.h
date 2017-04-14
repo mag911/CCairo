b055a01378bfd934ada9a163739e3253dec03952
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-15 06:54:23 +1000

    Add #include <sys/types.h>

--- cairo_bridging.h
+++ cairo_bridging.h
@@ -9,6 +9,7 @@ struct _cairo_font_options {};
 struct _cairo_region {};
 
 #define GLIB_DISABLE_DEPRECATION_WARNINGS
+#include <sys/types.h>
 #include <cairo.h>
 #include <cairo-deprecated.h>
 #include <cairo-features.h>
