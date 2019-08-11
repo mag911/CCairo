788da74ec7ab43496286e964492fdc93181c4ded
Rene Hexel <r.hexel@griffith.edu.au>
2019-08-12 09:12:30 +1000

    s/libcairo2.0-dev/libcairo2-dev/

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(name: "CCairo",
 	pkgConfig: "cairo glib-2.0 gio-unix-2.0",
 	providers: [
 		.brew(["cairo", "glib", "glib-networking", "gobject-introspection"]),
-		.apt(["libcairo2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+		.apt(["libcairo2-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	]
 )
